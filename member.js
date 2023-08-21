5a1d5dcf375b9c7de293ac985e4e97b9e8cd9f8d
michaeldegroeve <137800298+michaeldegroeve@users.noreply.github.com>
2023-08-21 10:08:00 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,18 +1,10 @@
 function skillsMember() {
-    var member = document.getElementById("member");
     var skills = document.getElementById("skills");
-    var projects = document.getElementById("projects");
-    var contact = document.getElementById("contact");
-    var memberButton = document.getElementById("memberButton");
-    var skillsButton = document.getElementById("skillsButton");
-    var projectsButton = document.getElementById("projectsButton");
-    var contactButton = document.getElementById("contactButton");
-    member.style.display = "block";
-    skills.style.display = "none";
-    projects.style.display = "none";
-    contact.style.display = "none";
-    memberButton.style.backgroundColor = "#333";
-    skillsButton.style.backgroundColor = "#111";
-    projectsButton.style.backgroundColor = "#111";
-    contactButton.style.backgroundColor = "#111";
+    var member = document.getElementById("member");
+    var memberValue = member.options[member.selectedIndex].value;
+    if (memberValue == "1") {
+        skills.style.display = "block";
+    } else {
+        skills.style.display = "none";
+    }
 }
